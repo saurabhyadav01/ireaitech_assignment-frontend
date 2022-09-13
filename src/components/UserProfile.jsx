00aeb9547b707a52b52f366f315b637f115c7965
saurabh yadav <72351102+saurabhyadav01@users.noreply.github.com>
2022-09-13 09:35:24 +0530

    Update UserProfile.jsx

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -32,7 +32,7 @@ console.log(id)
   const EditHandler = () => {
    
     console.log(token.token)
-      axios.patch(`https://web-elight.herokuapp.com/users/${id}` , data,{ headers: {"Authorization" : `Bearer ${token.token}`}}).then((res)=>{
+      axios.patch(`https://iraitech-assignment-db.herokuapp.com/users/${id}` , data,{ headers: {"Authorization" : `Bearer ${token.token}`}}).then((res)=>{
         console.log(res.data)
         
         toast.success("User Updated!!",{position:"top-center"})
